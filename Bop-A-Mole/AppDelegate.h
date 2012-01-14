9d3252807fdfaba3aa0b0fec989b1252e9739774
Hunter Francis <francis1122@gmail.com>
2012-01-14 00:07:33 -0800

    Initial Commit

---
+++ Bop-A-Mole/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Bop-A-Mole
+//
+//  Created by John Wilson on 1/14/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Bop-A-Mole/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Bop-A-Mole
+//
+//  Created by John Wilson on 1/14/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
